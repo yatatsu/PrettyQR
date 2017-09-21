26688fb81399b81c443b0455562005c9ce82e577
yatatsu <yatatsukitagawa@gmail.com>
2017-09-22 08:15:06 +0900

    feat(reader): Add barcode reader from googlesample

--- app/src/main/java/com/github/yatatsu/prettyqr/reader/ReaderFragment.kt
+++ app/src/main/java/com/github/yatatsu/prettyqr/reader/ReaderFragment.kt
@@ -1,30 +1,48 @@
 package com.github.yatatsu.prettyqr.reader
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.Camera
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.github.yatatsu.prettyqr.R
 import com.github.yatatsu.prettyqr.reader.ReaderFragmentPermissionsDispatcher.onRequestPermissionsResult
 import com.github.yatatsu.prettyqr.reader.ReaderFragmentPermissionsDispatcher.startCameraSafelyWithCheck
-import com.google.android.cameraview.CameraView
-import com.google.android.cameraview.CameraView.Callback
-import permissions.dispatcher.*
+import com.github.yatatsu.prettyqr.reader.camera.CameraSource
+import com.github.yatatsu.prettyqr.reader.camera.CameraSourcePreview
+import com.github.yatatsu.prettyqr.reader.camera.GraphicOverlay
+import com.google.android.gms.vision.MultiProcessor
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnNeverAskAgain
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.OnShowRationale
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.RuntimePermissions
 import timber.log.Timber
+import java.io.IOException
 
 
 @RuntimePermissions() class ReaderFragment : Fragment() {
 
-  private lateinit var cameraView: CameraView
+  private var cameraSource: CameraSource? = null
+  private lateinit var preview: CameraSourcePreview
+    private lateinit var graphicOverlay: GraphicOverlay<BarcodeGraphic>
 
   override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
       savedInstanceState: Bundle?): View? {
     return inflater!!.inflate(R.layout.fragment_reader, container, false)
         .also {
-          cameraView = it.findViewById<CameraView>(R.id.camera)
-              .apply { addCallback(cameraCallback) }
+          preview = it.findViewById(R.id.camera)
+          graphicOverlay = it.findViewById(R.id.graphicOverlay)
         }
   }
 
@@ -34,7 +52,7 @@ import timber.log.Timber
   }
 
   override fun onPause() {
-    cameraView.stop()
+    preview.stop()
     super.onPause()
   }
 
@@ -44,18 +62,22 @@ import timber.log.Timber
     onRequestPermissionsResult(this, requestCode, grantResults)
   }
 
-  private var cameraCallback = object : Callback() {
-    override fun onCameraOpened(cameraView: CameraView?) {
-      Timber.d("onCameraOpened")
-    }
-
-    override fun onCameraClosed(cameraView: CameraView?) {
-      Timber.d("onCameraClosed")
-    }
-  }
-
+  @SuppressLint("MissingPermission")
   @NeedsPermission(Manifest.permission.CAMERA) fun startCameraSafely() {
-    cameraView.start()
+    Timber.d("start camera")
+    if (cameraSource == null) {
+      createDetector(true, false)
+    }
+    cameraSource?.let {
+      try {
+        preview.start(cameraSource, graphicOverlay)
+      } catch (e: IOException) {
+        Timber.e(e, "Unable to start camera source.")
+        cameraSource?.release()
+        cameraSource = null
+      }
+
+    }
   }
 
   @OnShowRationale(Manifest.permission.CAMERA)
@@ -71,5 +93,53 @@ import timber.log.Timber
   @OnNeverAskAgain(Manifest.permission.CAMERA)
   fun onNeverAskAgainForCamera() {
     // TODO
+  }
+
+  private fun createDetector(autoFocus: Boolean, useFlash: Boolean) {
+    Timber.d("start creating detector")
+    val detector = BarcodeDetector.Builder(context)
+        .setBarcodeFormats(Barcode.QR_CODE)
+        .build()
+    val barcodeFactory = BarcodeTrackerFactory(graphicOverlay)
+    detector.setProcessor(
+        MultiProcessor.Builder<Barcode>(barcodeFactory).build())
+
+    if (!detector.isOperational) {
+      // Note: The first time that an app using the barcode or face API is installed on a
+      // device, GMS will download a native libraries to the device in order to do detection.
+      // Usually this completes before the app is run for the first time.  But if that
+      // download has not yet completed, then the above call will not detect any barcodes
+      // and/or faces.
+      //
+      // isOperational() can be used to check if the required native libraries are currently
+      // available.  The detectors will automatically become operational once the library
+      // downloads complete on device.
+      Timber.d("Detector dependencies are not yet available.")
+
+      val lowStorageFilter = IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW)
+      val hasLowStorage = context?.registerReceiver(null, lowStorageFilter) != null
+
+      if (hasLowStorage) {
+        Toast.makeText(context, "low storage error", Toast.LENGTH_LONG).show()
+        Timber.w("low storage error")
+      }
+    }
+
+    val builder = CameraSource.Builder(context.applicationContext, detector)
+        .setFacing(CameraSource.CAMERA_FACING_BACK)
+        .setRequestedPreviewSize(1600, 1024)
+        .setRequestedFps(15.0f)
+
+    // make sure that auto focus is an available option
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+      builder.setFocusMode(
+          if (autoFocus) Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE else null)
+    }
+
+    cameraSource = builder
+        .setFlashMode(if (useFlash) Camera.Parameters.FLASH_MODE_TORCH else null)
+        .build()
+
+    Timber.d("create camera source")
   }
 }
